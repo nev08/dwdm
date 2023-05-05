d00e2d1433a7fcb139295f45f4af56161a547f6b
nev08 <113408645+nev08@users.noreply.github.com>
2023-05-05 13:27:12 +0530

    Add files via upload

---
+++ subtraction.R
@@ -0,0 +1,9 @@
+a <- readline(prompt = "Enter first number:")
+a <- as.integer(a)
+
+b <- readline(prompt = "Enter second number:")
+b <- as.integer(b)
+
+difference = a - b
+
+print(paste("difference of two numbers is: ", difference))
