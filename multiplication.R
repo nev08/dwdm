55ad652e2066160a183eb19bfdb972ef44793558
nev08 <113408645+nev08@users.noreply.github.com>
2023-05-05 22:01:51 +0530

    Add files via upload

---
+++ multiplication.R
@@ -0,0 +1,9 @@
+a <- readline(prompt = "Enter first number:")
+a <- as.integer(a)
+
+b <- readline(prompt = "Enter second number:")
+b <- as.integer(b)
+
+mul = a * b
+
+print(paste("Product of two numbers is: ", mul))
