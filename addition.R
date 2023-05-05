ae89efff98d77bb49b9b2a63ce2061a6b185460c
nev08 <113408645+nev08@users.noreply.github.com>
2023-05-05 13:27:50 +0530

    Add files via upload

---
+++ addition.R
@@ -0,0 +1,9 @@
+a <- readline(prompt = "Enter first number:")
+a <- as.integer(a)
+
+b <- readline(prompt = "Enter second number:")
+b <- as.integer(b)
+
+sum = a + b
+
+print(paste("Sum of two numbers is: ", sum))
