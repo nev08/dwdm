34eb40262b092adadc645e3a569c93745151ed12
nev08 <113408645+nev08@users.noreply.github.com>
2023-05-05 13:28:25 +0530

    Add files via upload

---
+++ division.R
@@ -0,0 +1,9 @@
+a <- readline(prompt = "Enter first number:")
+a <- as.integer(a)
+
+b <- readline(prompt = "Enter second number:")
+b <- as.integer(b)
+
+div = a / b
+
+print(paste("Division of two numbers is: ", div))
